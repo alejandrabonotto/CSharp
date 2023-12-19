547931896d82cddda04c99c8ffaa9cd1cf126f77
Alejandra Bonotto <110475980+alejandrabonotto@users.noreply.github.com>
2023-12-19 14:16:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_A
+{
+    abstract class Student
+    {
+        
+        public abstract void study();
+
+    }
+
+    class RegularStudent : Student
+    {
+        
+        public override void study()
+        {
+            Console.WriteLine("I am a regular student");
+        }
+
+    }
+
+    class FasttrackStudent : Student
+    {
+        public override void study()
+        {
+            Console.WriteLine("I am a Fast track Student");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("My name: Alejandra Bonotto Farias Franco");
+            Console.WriteLine("My Student Number: 301285530");
+
+            Student s1 = new RegularStudent();
+            s1.study();
+
+            Student s2 = new FasttrackStudent();
+            s2.study();
+        }
+    }
+
+    
+}
